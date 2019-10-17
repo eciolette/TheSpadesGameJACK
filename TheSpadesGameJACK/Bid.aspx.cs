15fdf946a4cac42749608dda04c8b850eb4860cf
step.jump.1017@gmail.com <step.jump.1017@gmail.com>
2019-10-17 19:51:38 -0400

    Update Bid.aspx.cs

--- TheSpadesGameJACK/Bid.aspx.cs
+++ TheSpadesGameJACK/Bid.aspx.cs
@@ -11,77 +11,93 @@ namespace TheSpadesGameJACK
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void Button0_Click(object sender, EventArgs e)
         {
+            Response.Redirect("Start.aspx");
+            
+            //LabelBid.Text = "/ 0 ";
 
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 1 ";
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 2 ";
         }
 
         protected void Button3_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            // LabelBid.Text = "/ 3 ";
         }
 
         protected void Button4_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            // LabelBid.Text = "/ 4 ";
         }
 
         protected void Button5_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 5 ";
         }
 
         protected void Button6_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 6 ";
         }
 
         protected void Button7_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 7 ";
         }
 
         protected void Button8_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 8 ";
         }
 
         protected void Button9_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 9 ";
         }
 
         protected void Button10_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 10 ";
         }
 
         protected void Button11_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            // LabelBid.Text = "/ 11 ";
         }
 
         protected void Button12_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 12 ";
         }
 
         protected void Button13_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
+            //LabelBid.Text = "/ 13 ";
         }
     }
 }
