a32ae93df6831e11181acc54fdc9929fb47a6ce0
eciolette <college@bheshop.com.br>
2019-10-17 19:56:51 -0400

    Redirect

--- TheSpadesGameJACK/Bid.aspx.cs
+++ TheSpadesGameJACK/Bid.aspx.cs
@@ -16,67 +16,67 @@ namespace TheSpadesGameJACK
 
         protected void Button0_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button3_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button4_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button5_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button6_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button7_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button8_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button9_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button10_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button11_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button12_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Start.aspx");
         }
 
         protected void Button13_Click(object sender, EventArgs e)
