9ec7a9f9d5b11c432b8cac1766ad75a32c6312df
eciolette <college@bheshop.com.br>
2019-10-17 20:08:31 -0400

    Update Bid.aspx.cs

--- TheSpadesGameJACK/Bid.aspx.cs
+++ TheSpadesGameJACK/Bid.aspx.cs
@@ -17,120 +17,79 @@ namespace TheSpadesGameJACK
         protected void Button0_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
-            
             //LabelBid.Text = "/ 0 ";
-
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 1 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 2 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button3_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             // LabelBid.Text = "/ 3 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button4_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             // LabelBid.Text = "/ 4 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button5_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 5 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button6_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 6 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button7_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 7 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button8_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 8 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button9_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 9 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button10_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 10 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button11_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             // LabelBid.Text = "/ 11 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button12_Click(object sender, EventArgs e)
         {
             Response.Redirect("Start.aspx");
-<<<<<<< HEAD
-=======
             //LabelBid.Text = "/ 12 ";
->>>>>>> f8682ba24acbb777208b4669348184d83c9ee005
         }
 
         protected void Button13_Click(object sender, EventArgs e)
